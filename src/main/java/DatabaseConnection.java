e4394532cdfb200fe6d6b158f85ab774dc3e150b
AV-NASH <avinash21213434@gmail.com>
2020-10-29 21:53:11 +0530

    Database connection successfully established

--- src/main/java/DatabaseConnection.java
+++ src/main/java/DatabaseConnection.java
@@ -6,8 +6,31 @@ import java.util.Enumeration;
 
 public class DatabaseConnection {
     public static void main(String[] args) {
-        System.out.println("welcome to employee payroll program");
+        String jdbcURL="jdbc:mysql://localhost:3306/payroll_service?useSSL=false";
+        String userName="root";
+        String password="Nuzumaki1@";
+        Connection connection;
+        try{
+            Class.forName("com.mysql.jdbc.Driver");
+            System.out.println("Driver laded");
+        } catch (ClassNotFoundException e) {
+            System.out.println("cnnot find classpath");
+        }
+        listDrivers();
+        try {
+            System.out.println("Connecting to "+jdbcURL);
+            connection=DriverManager.getConnection(jdbcURL);
+            System.out.println("connection successful");
+        } catch (SQLException throwables) {
+            System.out.println("connection failed");
+        }
     }
-
+    private static void listDrivers() {
+        Enumeration<Driver> driverList= DriverManager.getDrivers();
+        while ((driverList.hasMoreElements())){
+            Driver driverClass=(Driver)driverList.nextElement();
+            System.out.println("  "+driverClass.getClass().getName());
+        }
+    }
 
 }
