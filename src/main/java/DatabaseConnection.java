43ef5665498f276d15e77518b80cbcb80a139176
AV-NASH <avinash21213434@gmail.com>
2020-10-29 21:47:54 +0530

    added welcome msg to master

---
+++ src/main/java/DatabaseConnection.java
@@ -0,0 +1,13 @@
+import java.sql.Connection;
+import java.sql.Driver;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Enumeration;
+
+public class DatabaseConnection {
+    public static void main(String[] args) {
+        System.out.println("welcome to employee payroll program");
+    }
+
+
+}
